9cbf568a9656b4510a5bd8208b2ad05ed3e8bed6
Sukrish Gokulakrishnan <enderslash@outlook.com>
2022-12-06 00:57:31 -0500

    Fixed 'super jump' with too much force
    Added waitTime (0.16) between jumps to remove 'super jumps' with too much force

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,6 +10,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Transform Posgroundcheck;
     [SerializeField] private LayerMask GroundLayer;
     [SerializeField] private float JumpForce = 800.0f;
+    [SerializeField] private float waitTime = 0.5f;
+    private float waitTimeSet = 0;
     private bool facingRight = true;
     private Rigidbody2D gBody;
     // Start is called before the first frame update
@@ -22,10 +24,14 @@ public class PlayerMovement : MonoBehaviour
         float hrz = Input.GetAxis("Horizontal");
         isGrounded = GroundCheck();
         // Code for jumping
-        if (isGrounded && Input.GetAxis("Jump") > 0)
+        if (isGrounded && waitTimeSet <= 0f && Input.GetAxis("Jump") > 0)
         {
             gBody.AddForce(new Vector2(0.0f, JumpForce));
+            waitTimeSet = waitTime;
             isGrounded = false;
+        } else if (waitTimeSet > 0)
+        {
+            waitTimeSet -= Time.deltaTime;
         }
 
         if (facingRight == false && hrz > 0)
